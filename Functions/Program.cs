db6fd2446b9231dc63819fee4014d5c82e933fc5
Gertu Piho <gertu.piho@tktk.ee>
2022-10-18 22:15:17 +0300

    Funktsioonid

---
+++ Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoise = Console.ReadLine();
+
+if(userChoise == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodgye();
+}
+
+
+
+//PrintHello();
+//PrintGoodgye();
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+static void PrintGoodgye()
+{
+    Console.WriteLine("See you later, aligator!");
+}
